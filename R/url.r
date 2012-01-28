5d5acbfb58e3fa3f8909aebe60c60bd2f9c196a5
hadley <h.wickham@gmail.com>
2012-01-28 08:46:49 -0600

    Make parse and build slightly capable

--- R/url.r
+++ R/url.r
@@ -13,6 +13,9 @@
 #'  \item username
 #'  \item password
 #' }
+#' parse_url("http://google.com/")
+#' parse_url("http://google.com:80/")
+#' parse_url("http://google.com:80/?a=1&b=2")
 parse_url <- function(url) {
   url <- as.character(url)
   stopifnot(length(url) == 1)
@@ -31,39 +34,69 @@ parse_url <- function(url) {
   netloc <- pull_off("^//([^/]*)/?")
   
   if (!is.null(netloc)) {
-    port <- str_match(netloc, ":([0-9]+)^")[, 2]
-    # username <- 
+    
+    pieces <- str_split(netloc, "@")[[1]]
+    if (length(pieces) == 1) {
+      username <- NULL
+      password <- NULL
+     
+      host <- pieces
+    } else {
+      user_pass <- str_split(pieces[[1]], ":")[[1]]
+      username <- user_pass[1]
+      password <- user_pass[2]
+      
+      host <- pieces[2]
+    }
+    
+    host_pieces <- str_split(pieces, ":")[[1]]
+    hostname <- host_pieces[1]
+    port <- host_pieces[2]
   } else {
     port <- username <- password <- hostname <- NULL
   }
   
   query <- pull_off("\\?(.*)$")
+  if (!is.null(query)) {
+    params <- vapply(str_split(query, "&")[[1]], str_split_fixed, "=", 2,
+      FUN.VALUE = character(2))
+    values <- type.convert(curlUnescape(params[2, ]), as.is = TRUE)
+    names <- params[1, ]
+    query <- setNames(as.list(values), names)
+  }  
   params <- pull_off(";(.*)$")
 
-  return(list(fragment = fragment, scheme = scheme, netloc = netloc, 
-    query = query, params = params, path = url))
+  return(list(scheme = scheme, hostname = hostname, port = port, path = url, 
+    query = query, params = params, username = username, password = password))
   
 }
 
-build_url <- function(scheme = "http", hostname, port = NULL, path, query = list(), params = list(), username = NULL, password = NULL) { 
-  if (length(params) == 0) return(url)
-  
+build_url <- function(hostname, path, scheme = "http", port = NULL, query = list(), params = list(), username = NULL, password = NULL) { 
+
   if (is.list(query)) {
-    escaped <- curlEscape(params)
-    query <- str_c(names(params), "=", escaped, collapse = "&")    
+    escaped <- curlEscape(query)
+    query <- str_c(names(query), "=", escaped, collapse = "&")    
   }
-  stopifnot(is.character(query))
-  stopifnot(length(query) == 1)
+  if (!is.null(query)) {
+    stopifnot(is.character(query), length(query) == 1)
+    query <- str_c("?", query)
+  }
   
-  str_c(url, "?", query)
+  if (!is.null(port)) {
+    port <- str_c(":", port)
+  }
+  
+  str_c(scheme, "://", hostname, path, query)
 }
 
 
-modify_url <- function(url, scheme, hostname, port, path, query, params, username, password) {
+modify_url <- function(url, scheme = NULL, hostname = NULL, port = NULL, path = NULL, query = NULL, params = NULL, username = NULL, password = NULL) {
   old <- parse_url(url)
-  new <- list(scheme = scheme, hostname = hostname, port = port, path = path,
-    query = query, params = params, username = username, password = password)
+  new <- compact(list(scheme = scheme, hostname = hostname, port = port, path = path,
+    query = query, params = params, username = username, password = password))
   
-  build_url(modifyList(old, new))
+  do.call("build_url", modifyList(old, new))
 }
 
+
+compact <- function(x) Filter(Negate(is.null), x)
