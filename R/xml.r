2c06f27e96c46ffc1eb162bc4caf2866351bbae9
hadley <h.wickham@gmail.com>
2012-01-28 08:01:45 -0600

    Flesh out xml helper a little more

--- R/xml.r
+++ R/xml.r
@@ -1,6 +1,17 @@
 "[.xml" <- function(x, i, ...) {
-  if (!is.character(i)) NextMethod()
-  
+  if (!is.xpath(i)) NextMethod()
+
+  getNodeSet(x, i)
   # otherwise do an xml search
   
 }
+
+
+#' Flags a string as an xpath expression.
+#'
+xpath <- function(x) {
+  stopifnot(length(x) == 1, is.character(x))
+  structure(x, "xpath")
+}
+
+is.xpath <- function(x) inherits(x, "xpath")
