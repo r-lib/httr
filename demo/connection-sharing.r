ec4c0b6e2b316ddb61a753836b6201018bd5565c
hadley <h.wickham@gmail.com>
2012-01-29 10:22:10 -0600

    Fix bugs in demo

--- demo/connection-sharing.r
+++ demo/connection-sharing.r
@@ -1,12 +1,10 @@
-tools::startDynamicHelp()
-port <- tools:::httpdPort
-test_server <- "http://requestb.in/1ejpzfj1"
+test_server <- "http://had.co.nz"
 
 # Create a new handle for every request - no connection sharing
 rowMeans(replicate(20, 
   GET(handle = handle(test_server), path = "index.html")$times))
 
-help_handle <- handle(test_server)
+test_handle <- handle(test_server)
 # Re use the same handle for multiple requests
 rowMeans(replicate(20, 
-  GET(handle = test_server, path = "index.html")$times))
+  GET(handle = test_handle, path = "index.html")$times))
