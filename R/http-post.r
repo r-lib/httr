9852cdf9d3e31be18d920d242128fd02bdc60d59
hadley <h.wickham@gmail.com>
2012-03-30 14:57:56 -0500

    More notes on post

--- R/http-post.r
+++ R/http-post.r
@@ -15,3 +15,20 @@ POST <- function(url = NULL, config = list(), body = NULL, ..., handle = NULL) {
 #  * raw content which server interprets
 #  * multipart/form-data: like mime, multiple named sections
 #  * application/x-www-form-urlencoded: url escaped params concatenated 
+
+is.connection <- function(x) inherit(x, "connection")
+# If is.connection(x) - base64 encode data and send as is
+# If is.raw(x) or is.character(x) and length(x) == 1 send as is
+# If is.raw(x) or is.character(x) and length(x) > 1 throw error
+# If is.list(x), 
+#   separate character/raw vs. connections
+#
+# Alternative is to have separate files and fields - but then still need
+# some way of sending raw bodies
+
+# encode_multipart argument?
+#  - if TRUE, use multipart encoding
+#  - if FALSE, use urlencoding
+
+# https://github.com/kennethreitz/requests/blob/develop/requests/packages/urllib3/filepost.py
+# https://github.com/kennethreitz/requests/blob/develop/requests/packages/urllib3/request.py#L82
