332096e4b9bb7659c6265aa51f21b53b19f06b81
hadley <h.wickham@gmail.com>
2012-03-30 11:31:24 -0500

    Tweak authenticate default arg

--- R/authenticate.r
+++ R/authenticate.r
@@ -4,12 +4,10 @@
 #' @param password password
 #' @param type type of HTTP authentication.  Should be one of the following
 #'   types supported by Curl: basic, digest, digest_ie, gssnegotiate,
-#'   ntlm, ntlm_vn, any, anysafe.  It defaults to anysafe, which will use 
-#'   authentication method that does not send the password in cleartext
-#'   (i.e. everything except http basic authetication).
+#'   ntlm, ntlm_vn, any.  It defaults to any.
 #' @export
 #' @family config
-authenticate <- function(user, password, type = "anysafe") {
+authenticate <- function(user, password, type = "any") {
   stopifnot(is.character(user), length(user) == 1)
   stopifnot(is.character(password), length(password) == 1)
   stopifnot(is.character(type), length(type) == 1)
