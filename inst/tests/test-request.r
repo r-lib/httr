32169de75f01847439598d88541719bc71ffb4f0
hadley <h.wickham@gmail.com>
2012-03-30 14:17:01 -0500

    Add headers test

--- inst/tests/test-request.r
+++ inst/tests/test-request.r
@@ -7,3 +7,16 @@ test_that("status codes returned as expected", {
   expect_equal(GET("http://httpbin.org/status/418")$status, 418)
   
 })
+
+test_that("headers returned as expected", {
+  round_trip <- function(...) {
+    req <- GET("http://httpbin.org/headers", add_headers(...))
+    headers <- fromJSON(req$text)$headers
+    names(headers) <- tolower(names(headers))
+    headers
+  }
+  
+  expect_equal(round_trip(a = 1)$a, "1")
+  expect_equal(round_trip(a = "a + b")$a, "a + b")
+  
+})
