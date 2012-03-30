28e154a98a3f7cdb936d2b585353a643642bcf7b
hadley <h.wickham@gmail.com>
2012-03-30 09:36:11 -0500

    Use cookies by default

--- R/request.r
+++ R/request.r
@@ -22,7 +22,6 @@ make_request <- function(action, handle, url, content, params, config = list()) 
   
   info <- last_request(handle)
   times <- request_times(handle)
-  
   headers <- as.list(hg$value())
   
   response(
@@ -30,7 +29,7 @@ make_request <- function(action, handle, url, content, params, config = list()) 
     handle = handle,
     status_code = headers$status,
     headers = headers,
-    # cookies = cookies,
+    cookies = parse_cookies(info$cookielist),
     text = content,
     times = times,
     config = config
@@ -100,4 +99,11 @@ request_times <- function(x) {
     starttransfer = req$starttransfer.time, 
     total = req$total.time)
 
+}
+
+parse_cookies <- function(x) {
+  if (length(x) == 0) return(list())
+  
+  pieces <- str_split_fixed(x, fixed("\t"), n = 7)
+  setNames(as.list(pieces[, 7]), pieces[, 6])
 }
