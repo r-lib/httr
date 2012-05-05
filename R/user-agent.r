e8970a48cab7a9e6b4c6edd0b59a5bfc9eb0023d
hadley <h.wickham@gmail.com>
2012-05-05 13:40:57 -0500

    Convenient way to set user agent

---
+++ R/user-agent.r
@@ -0,0 +1,15 @@
+#' Set user agent.
+#' 
+#' Override the default RCurl user agent of \code{NULL}
+#'
+#' @param agent string giving user agent
+#' @export
+#' @family config
+#' @examples
+#' GET("http://httpbin.org/user-agent")
+#' GET("http://httpbin.org/user-agent", user_agent("httr"))
+user_agent <- function(agent) {
+  stopifnot(is.character(agent), length(agent) == 1)
+
+  config(useragent = agent)
+}
