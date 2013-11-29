0464dfb5190aba00796551b9b8df8eeefa2d60bd
hadley <h.wickham@gmail.com>
2013-11-29 12:34:03 -0600

    First stab at new oauth ref classes

---
+++ R/oauth-refresh.R
@@ -0,0 +1,30 @@
+#' Refresh an OAuth 2.0 credential.
+#'
+#' Refreshes the given token, and returns a new credential with a
+#' valid access_token. Based on:
+#'   https://developers.google.com/accounts/docs/OAuth2InstalledApp#refresh
+#'
+#' @family OAuth
+#' @param endpoint
+#' @param app
+#' @param credential
+#' @return a new credential
+#' @export
+refresh_oauth2.0 <- function(endpoint, app, credentials) {
+  if (is.null(credentials$refresh_token)) {
+    stop("Invalid credential: missing refresh token")
+  }
+  refresh_url <- endpoint$access
+  body <- list(
+    refresh_token = credential$refresh_token,
+    client_id = app$key,
+    client_secret = app$secret,
+    grant_type = "refresh_token"
+  )
+  
+  response <- POST(refresh_url, body = body, multipart = FALSE)
+  stop_for_status(response)
+
+  refresh_data <- content(response)
+  modifyList(credentials, refresh_data)
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onAttach <- function(libname, pkgname) {  
   op <- options()
   op.dplyr <- list(
-    httr_oob_default = FALSE
+    httr_oob_default = FALSE,
+    httr_oauth_cache = NA
   )
   toset <- !(names(op.dplyr) %in% names(op))
   if(any(toset)) options(op.dplyr[toset])
