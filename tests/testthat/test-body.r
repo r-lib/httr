e35940a3d7f2968a8ec1ab6b18e24484581700c3
hadley <h.wickham@gmail.com>
2014-02-27 14:33:03 -0600

    Improve tests for http body

--- tests/testthat/test-body.r
+++ tests/testthat/test-body.r
@@ -1,14 +1,14 @@
-context("Send data")
+context("Body")
 
 round_trip <- function(method, body = NULL, ...) {
   url <- paste0("http://httpbin.org/", tolower(method))
   request <- match.fun(toupper(method))
   content(request(url, body = body, ...))
 }
-methods <- c("POST", "PUT")
+methods <- c("POST", "PUT", "PATCH")
 
-citation <- upload_file(system.file("CITATION"))
-citation_val <- c(readLines(system.file("CITATION")), "")
+data_path <- upload_file("data.txt")
+data <- c(readLines("data.txt"), "")
 
 test_that("empty body gives empty data element", {
   for(method in methods) {
@@ -24,9 +24,17 @@ test_that("string in body gives same string in data element", {
   }
 })
 
-test_that("named list matches form results", {
+test_that("named list matches form results (multipart = TRUE)", {
   for(method in methods) {
-    out <- round_trip(method, list(a = 1, b = 2))
+    out <- round_trip(method, list(a = 1, b = 2), multipart = TRUE)
+    expect_equal(out$form$a, "1", info = method)
+    expect_equal(out$form$b, "2", info = method)
+  }
+})
+
+test_that("named list matches form results (multipart = FALSE)", {
+  for(method in methods) {
+    out <- round_trip(method, list(a = 1, b = 2), multipart = FALSE)
     expect_equal(out$form$a, "1", info = method)
     expect_equal(out$form$b, "2", info = method)
   }
@@ -34,9 +42,9 @@ test_that("named list matches form results", {
 
 test_that("file and form vals mixed give form and data elements", {
   for(method in methods) {
-    out <- round_trip(method, list(y = citation, a = 1))
+    out <- round_trip(method, list(y = data_path, a = 1))
     expect_equal(out$form$a, "1", info = method)
-    expect_equal(str_split(out$file$y, "\n")[[1]], citation_val,
+    expect_equal(str_split(out$file$y, "\n")[[1]], data,
       info = method)
   }
 })
