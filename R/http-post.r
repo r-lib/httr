5b6341e16b74ea5cc2a6fd3114eff5e3ff1a9af7
hadley <h.wickham@gmail.com>
2014-03-18 11:01:33 -0500

    Improve docs for body parameter

--- R/http-post.r
+++ R/http-post.r
@@ -1,13 +1,16 @@
 #' POST file to a server.
 #'
 #' @inheritParams GET
-#' @param body Use \code{NULL} for an empty body, a length-one character or
-#    raw vector, or a named of list of elements to go in the body of the post
-#'   file. Each component should either be a character value or the object
-#'   returned by \code{\link[RCurl]{fileUpload}} (if you want to upload a
-#'   file).  If \code{multipart} is \code{FALSE} elements will be escaped
-#'   automatically - if the values have already been escaped, then use
-#'   `I` to prevent double-escaping.
+#' @param body One of the following:
+#'   \itemize{
+#'     \item \code{FALSE}: No body
+#'     \item \code{NULL}: An empty body
+#'     \item A length one character or raw vector: sent as is in body.
+#'     \item A named list: Each component should either be a string
+#'       or the object returned by \code{\link[RCurl]{fileUpload}}
+#'      (if you want to upload a file). Strings will be escaped
+#'      automatically: use \code{I()} to prevent double-escaping.
+#'   }
 #' @param multipart Should the form be send as multipart/form-data
 #'   (\code{TRUE}), or application/x-www-form-urlencoded (\code{FALSE}).
 #'   Files can not be uploaded when \code{FALSE}.
