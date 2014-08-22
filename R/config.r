17250931eeb2a4d18b5fb13cceb754c5e1608cbb
hadley <h.wickham@gmail.com>
2014-08-22 14:47:43 -0500

    Add better config docs. Closes #116. Closes #117.

--- R/config.r
+++ R/config.r
@@ -15,8 +15,27 @@
 #'  \code{\link{with_config}} to temporarily run code with set options.
 #' @family config
 #' @family ways to set configuration
+#' @seealso All known available options are listed in \code{\link{httr_options}}
 #' @param ... named Curl options.
 #' @export
+#' @examples
+#' # There are a number of ways to modify the configuration of a request
+#' # * you can add directly to a request
+#' HEAD("https://www.google.com", verbose())
+#'
+#' # * you can wrap with with_config()
+#' with_config(verbose(), HEAD("https://www.google.com"))
+#'
+#' # * you can set global with set_config()
+#' set_config(verbose())
+#' HEAD("https://www.google.com")
+#' reset_config(old)
+#' HEAD("https://www.google.com")
+#'
+#' # If available, you should use a friendly httr wrapper over RCurl
+#' # options. But you can pass Curl options (as listed in httr_options())
+#' # in config
+#' HEAD("https://www.google.com/", config(verbose = 1L))
 config <- function(...) {
   options <- list(...)
 
