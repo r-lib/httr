25eebe3b90aa9336cbac3744cc1c317e0aec1494
hadley <h.wickham@gmail.com>
2014-08-23 14:37:51 -0500

    Import C function from RCurl to avoid R CMD check problem

--- src/writer.c
+++ src/writer.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <R.h>
+#include <Rdefines.h>
 
 // From http://stackoverflow.com/questions/17329288/
 // License: http://creativecommons.org/licenses/by-sa/3.0/
@@ -6,4 +8,18 @@
 size_t writer(void *buffer, size_t size, size_t nmemb, void *stream) {
   fwrite(buffer, size, nmemb, (FILE *) stream);
   return size * nmemb;
+}
+
+// From: RCurl
+// BSD_3_clause
+// YEAR: 2001-2014
+// COPYRIGHT HOLDER: Duncan Temple Lang
+// ORGANIZATION: Bell Labs, Lucent Technologies; University of California
+void close_file(SEXP r_file) {
+  FILE *f = (FILE *) R_ExternalPtrAddr(r_file);
+  if (f) {
+    fflush(f);
+    fclose(f);
+    R_SetExternalPtrAddr(r_file, NULL);
+  }
 }
