4a3640f49663e434b7b81b1b9871e74e26cbbe08
Bogstag <krister@bogstag.se>
2014-09-13 17:21:53 +0200

    Updated style and added news
    Fixed some styles issues and added a bullet point to the news. Also
    fixed a typo in params explanation.

--- demo/oauth1-withings.r
+++ demo/oauth1-withings.r
@@ -5,16 +5,16 @@ withings <- oauth_endpoint("request_token", "authorize", "access_token", base_ur
 
 # 2. Register an application at https://account.withings.com/connectionuser/account_create
 #    Insert your values below.
-withingsapp <- oauth_app("withings", key="xxx", secret ="xxx")
+withingsapp <- oauth_app("withings", key = "xxx", secret = "xxx")
 
 # 3. Get OAuth credentials
-withings_token <- oauth1.0_token(withings, withingsapp, as_header=FALSE)
+withings_token <- oauth1.0_token(withings, withingsapp, as_header = FALSE)
 
 # 4. Use API
 #   You get userid in Step 2: http://oauth.withings.com/api
-req <- GET("http://wbsapi.withings.net/measure"
-           , query = list(action = "getmeas", userid = 1234567)
-           , config(token = withings_token))
+req <- GET("http://wbsapi.withings.net/measure",
+           query = list(action = "getmeas", userid = 1234567),
+           config(token = withings_token))
 
 stop_for_status(req)
 content(req)
