48ef4030c907a11926b852ceb5975986f7c043f2
hadley <h.wickham@gmail.com>
2014-10-06 12:42:24 -0500

    Stream handling. Closes #143

--- src/writer.c
+++ src/writer.c
@@ -5,7 +5,7 @@
 // From http://stackoverflow.com/questions/17329288/
 // License: http://creativecommons.org/licenses/by-sa/3.0/
 // Author: Ast Derek, https://github.com/AstDerek
-size_t writer(void *buffer, size_t size, size_t nmemb, void *stream) {
+size_t writer(void *buffer, size_t size, size_t nmemb, void* stream) {
   fwrite(buffer, size, nmemb, (FILE *) stream);
   return size * nmemb;
 }
@@ -23,3 +23,25 @@ void close_file(SEXP r_file) {
     R_SetExternalPtrAddr(r_file, NULL);
   }
 }
+
+
+// Adapted from RCurl
+// BSD_3_clause
+// YEAR: 2001-2014
+// COPYRIGHT HOLDER: Duncan Temple Lang
+// ORGANIZATION: Bell Labs, Lucent Technologies; University of California
+size_t write_callback(void *buffer, size_t size, size_t nmemb, void* fun) {
+  if (TYPEOF(fun) != CLOSXP) return 0;
+
+  // Convert buffer into a raw vector
+  SEXP bytes = PROTECT(allocVector(RAWSXP, size * nmemb));
+  memcpy(RAW(bytes), buffer, size * nmemb);
+
+  // Call fun with bytes as first argument
+  SEXP call = PROTECT(LCONS(fun, LCONS(bytes, R_NilValue)));
+  Rf_eval(call, R_GlobalEnv);
+  UNPROTECT(2);
+
+  return size * nmemb;
+}
+
