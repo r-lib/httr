594c14b190686821f3f5e82b7c22a57ac3b8d2c9
hadley <h.wickham@gmail.com>
2014-10-09 09:14:26 -0500

    Add revoke_all. Closes #77

--- R/oauth-token-utils.R
+++ R/oauth-token-utils.R
@@ -1,3 +1,36 @@
+#' Revoke all OAuth tokens in the cache.
+#'
+#' Use this function if you think that your token may have been compromised,
+#' e.g. you accidentally uploaded the cache file to github. It's not possible
+#' to automatically revoke all tokens - this function will warn when it can't.
+#'
+#' @param cache_path Path to cache file. Defaults to `.httr-oauth` in
+#'   current directory.
+#' @export
+revoke_all <- function(cache_path = NA) {
+  cache_path <- use_cache(cache_path)
+
+  if (is.null(cache_path)) {
+    stop("Can't find cache")
+  }
+
+  tokens <- load_cache(cache_path)
+
+  cant_revoke <- vapply(tokens, function(x) is.null(x$endpoint$revoke),
+    logical(1))
+  if (any(cant_revoke)) {
+    manual <- tokens[cant_revoke]
+    apps <- vapply(manual, function(x) {
+      paste0(x$app$appname, " (", x$app$key, ")")
+    }, character(1), USE.NAMES = FALSE)
+    warning("Can't revoke the following tokens automatically: ",
+      paste0(apps, collapse = ", "), call. = FALSE)
+  }
+
+  lapply(tokens, function(x) try(revoke_oauth2.0(x)))
+  invisible(TRUE)
+}
+
 revoke_oauth2.0 <- function(endpoint, credentials) {
   if (is.null(endpoint$revoke)) {
     stop("No revoke endpoint", call. = FALSE)
