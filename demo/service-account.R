dc9fc1e81b0ab11576fee612745e2f91d85c1bcf
hadley <h.wickham@gmail.com>
2015-12-18 14:51:09 -0600

    Starting working on service account demo

---
+++ demo/service-account.R
@@ -0,0 +1,17 @@
+
+# 1. Find OAuth settings for google:
+#    https://developers.google.com/accounts/docs/OAuth2InstalledApp
+oauth_endpoints("google")
+
+# 2. Register an project at https://cloud.google.com/console#/project
+
+# 3. Navigate to API Manager, then credentials. Create a new
+#    "service account key". This will generate a JSON file that you need to
+#    save in a secure location. This file is equivalent to a username +
+#    password pair.
+
+token <- oauth_service_token(
+  oauth_endpoints("google"),
+  jsonlite::fromJSON("demo/service-account.json"),
+  "https://www.googleapis.com/auth/userinfo.profile"
+)
