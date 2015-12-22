147cbd2d5dfa3249da1f71ca34f5ebd9c0452a56
Saurabh Suman <sasuiitd@users.noreply.github.com>
2015-12-22 13:26:31 +0530

    Update oauth2-github.r

--- demo/oauth2-github.r
+++ demo/oauth2-github.r
@@ -10,8 +10,8 @@ oauth_endpoints("github")
 #
 #    Replace your key and secret below.
 myapp <- oauth_app("github",
-  key = "56b637a5baffac62cad9",
-  secret = "8e107541ae1791259e9987d544ca568633da2ebf")
+  key = "4fe03fc4adcf34ada84c",
+  secret = "76e76112ce859733a4a5c79276d83074fcc03108")
 
 # 3. Get OAuth credentials
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
