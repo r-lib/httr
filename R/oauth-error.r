72145e2a334752468beee93e2b111c40a36eba18
Nathan Goulding <nathan.goulding@piano.io>
2016-01-07 15:39:42 -0500

    update to content

--- R/oauth-error.r
+++ R/oauth-error.r
@@ -17,7 +17,7 @@ oauth2.0_errors <- c(
 has_oauth2.0_error <- function(response) {
   if (status_code(response) %in% oauth2.0_error_codes) {
     content <- content(response)
-    if (content["error"] %in% oauth2.0_errors) {
+    if (content$error %in% oauth2.0_errors) {
       return(TRUE)
     }
   }
