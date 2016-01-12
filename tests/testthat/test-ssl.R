7488e86b6648f22a66404b8ca7bea0528334a3b9
hadley <h.wickham@gmail.com>
2016-01-12 10:33:58 -0600

    Switch ssl examples

--- tests/testthat/test-ssl.R
+++ tests/testthat/test-ssl.R
@@ -2,5 +2,5 @@ context("SSL")
 
 test_that("SSL certificates verified as expected", {
   expect_equal(status_code(GET("https://google.com")), 200L)
-  expect_equal(status_code(GET("https://login.salesforce.com")), 200L)
+  expect_equal(status_code(GET("https://firefox.com")), 200L)
 })
