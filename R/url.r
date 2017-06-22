d41bf9ea8ee56aa24107e1ad400264c36f98a3e1
jennybc <jenny@stat.ubc.ca>
2017-06-22 08:23:08 -0700

    Recognize more forms of empty query; closes #451

--- R/url.r
+++ R/url.r
@@ -120,11 +120,11 @@ build_url <- function(url) {
   }
 
   if (is.list(url$query)) {
-    query <- compose_query(url$query)
+    query <- compose_query(compact(url$query))
   } else {
     query <- url$query
   }
-  if (!is.null(query)) {
+  if (!is.null(query) && nzchar(query)) {
     stopifnot(is.character(query), length(query) == 1)
     query <- paste0("?", query)
   }
