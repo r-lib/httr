d17a69ae585207d6fc0a65f593055a1e423e7499
Peter Hartman <me@peterhartman.co.uk>
2018-12-04 16:46:04 +0000

    Fix basic auth on OAuth2 client credentials flow (#485)
    And vimeo demo to use OAuth 2.0
    
    Fixes #484. Fixes #491.

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,11 @@
 # httr 1.3.1.9000
 
+* The Vimeo demo (and `oauth_endpoints()`) have been updated to use OAuth 2.0
+  (#491).
+
+* `init_oauth2.0()` passes `use_basic_auth` onwards, enabling 
+   basic authentication for OAuth 2.0 (@peterhartman, #484).
+
 * `cache_info()` now handles un-named flags, as illustrated by "private" when
   the server returns "private, max-age = 0".
 
--- demo/oauth2-vimeo.r
+++ demo/oauth2-vimeo.r
@@ -11,11 +11,15 @@ myapp <- oauth_app("vimeo",
   secret = "51ab8cb2cbb8b7eb")
 
 # 3. Get OAuth credentials
-vimeo_token <- oauth1.0_token(oauth_endpoints("vimeo"), myapp)
+vimeo_token <- oauth2.0_token(
+  oauth_endpoints("vimeo"), myapp,
+  as_header = TRUE,
+  use_basic_auth = TRUE
+)
 
 # 4. Use API
-req <- GET("https://vimeo.com/api/rest/v2",
-  query = list(method = "vimeo.videos.getAll", format = "json"),
-  config(token = vimeo_token))
+req <- GET("https://api.vimeo.com/me/videos",
+  config(token = vimeo_token)
+)
 stop_for_status(req)
-str(content(req))
+str(jsonlite::fromJSON(content(req, "text")))
