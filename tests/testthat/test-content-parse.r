404d0b106b3205634ba332cff42cded1af66f148
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-02-22 08:59:49 -0800

    Skip test in absence of "latin1" alias
    Closes #733

--- tests/testthat/test-content-parse.r
+++ tests/testthat/test-content-parse.r
@@ -14,8 +14,10 @@ test_that("invalid encoding/type gets UTF-8", {
 })
 
 test_that("encoding guessed from type", {
+  skip_if_not("latin1" %in% tolower(iconvlist()), "latin1 alias not present")
   expect_equal(guess_encoding(NULL, "text/plain; charset=latin1"), "latin1")
+})
 
-  # But overridden by type
+test_that("explicit type overrides charset", {
   expect_equal(guess_encoding("UTF-8", "text/plain; charset=latin1"), "UTF-8")
 })
