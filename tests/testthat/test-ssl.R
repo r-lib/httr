9bc9f8a51e4b546b18c8b615e1a6db8a7a6a4978
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-02-23 08:54:26 -0800

    Add another skip_on_cran()
    Caused submission to fail on debian (but not Windows), so just intermittent bad luck

--- tests/testthat/test-ssl.R
+++ tests/testthat/test-ssl.R
@@ -1,6 +1,7 @@
 context("SSL")
 
 test_that("SSL certificates verified as expected", {
+  skip_on_cran()
   expect_equal(status_code(GET("https://google.com")), 200L)
   expect_equal(status_code(GET("https://firefox.com")), 200L)
 })
